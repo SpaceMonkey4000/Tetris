5891b71d04d23484d81b5657eb5b53c72dcb22d4
Axel <axel@puppy.local>
2018-07-14 16:55:55 -0700

    test

--- Tetris/main.swift
+++ Tetris/main.swift
@@ -6,8 +6,6 @@ let gridSizeY = 24
 // The number of rows that are hidden beyond the top of the scren.
 let hiddenMatrixRows = 3.8
 
-// test
-
 let pikachu = texture(named: "Pikachu")
 let pants = texture(named: "Pants")
 
