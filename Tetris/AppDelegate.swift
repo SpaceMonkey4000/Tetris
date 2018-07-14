9ff041c389daf37c3a00204020eecf6c2ab8eecf
Drew Olbrich <drew.olbrich@gmail.com>
2018-07-14 11:30:19 -0700

    Initial Commit

---
+++ Tetris/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Tetris
+//
+//  Created by Drew Olbrich on 7/14/18.
+//  Copyright © 2018 Retroactive Fiasco. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
