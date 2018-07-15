9d8e0f12a702539ebf4a683caf58309af57a9d73
Axel <axel@puppy.local>
2018-07-14 17:42:03 -0700

    poopy pants

--- Tetris/main.swift
+++ Tetris/main.swift
@@ -6,33 +6,33 @@ let gridSizeY = 24
 // The number of rows that are hidden beyond the top of the scren.
 let hiddenMatrixRows = 3.8
 
-let pikachu = texture(named: "Pikachu")
-let pants = texture(named: "Pants")
+let Garbage = texture(named: "Garbageicon")
+var fallspeed = 20
 
-var pantsCounter = 0
+var fallCounter = 0
 var pantsX = 2
 var pantsY = gridSizeY - 1
 
 // This function is called once, before the game starts.
 func first() {
     // The lower left corner of the grid is coordinate 0, 0.
+    // This makes a row of tiles appear at the bottom of the screen.
+    for a in 0..<10 {
+        setTexture(Garbage, x: a, y: 0)
+    }
 
-    setTexture(pants, x: 0, y: 3)
-    setTexture(pants, x: 0, y: 4)
-    setTexture(pants, x: 1, y: 4)
-    setTexture(pikachu, x: 2, y: 5)
-    clearTexture(x: 0, y: 3)
+
 }
 
 // This function is called 60 times per second.
 func update() {
-    fallPants()
+    fallMino()
 }
 
-func fallPants() {
-    pantsCounter -= 1
-    if pantsCounter < 0 {
-        pantsCounter = 10
+func fallMino() {
+    fallCounter -= 1
+    if fallCounter < 0 {
+        fallCounter = fallspeed
 
         if hasTextureAt(x: pantsX, y: pantsY - 1) {
             return
@@ -40,7 +40,7 @@ func fallPants() {
 
         clearTexture(x: pantsX, y: pantsY)
         pantsY -= 1
-        setTexture(pants, x: pantsX, y: pantsY)
+        setTexture(Garbage, x: pantsX, y: pantsY)
     }
 }
 
