a0350e7299c30649e984e39a9306fdf678f700d5
Drew Olbrich <drew.olbrich@gmail.com>
2018-07-15 15:48:30 -0700

    Rename Tetrimino to Tetromino

---
+++ Tetris/Tetrominos.swift
@@ -0,0 +1,8 @@
+
+import Foundation
+
+func createJTetromino() -> Tetromino {
+    return Tetromino(shapes: [jNorth, jEast, jSouth, jWest])
+}
+
+// TODO: Add create functions for all other tetrominos.
