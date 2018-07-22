3b86303883d0ea56725ab7fc92fd4658455aac64
Drew Olbrich <milpitas@users.noreply.github.com>
2018-07-22 16:48:08 -0700

    Replace Grid.height with Grid.scale

--- Tetris/Secret Dad Stuff/Grid.swift
+++ Tetris/Secret Dad Stuff/Grid.swift
@@ -8,16 +8,18 @@ class Grid {
     private let cellsY: Int
     
     // Center of the grid, in normalized window coordinates, with the origin at the center of the window.
+    // The bottom of the window is -1 and the top is 1.
     private let center: CGPoint
     
-    // Size of the grid, in normalized window coordinates.
-    private let size: CGPoint
+    // Scale of the grid, relative to the size of the scene as defined in grid cells.
+    // If scale is 1, the size of the grid's cells will match those of the scene.
+    private let scale: CGPoint
         
-    init(cellsX: Int, cellsY: Int, center: CGPoint, height: GGFloat) {
+    init(cellsX: Int, cellsY: Int, center: CGPoint, scale: GGFloat) {
         self.cellsX = cellsX
         self.cellsY = cellsY
         self.center = center
-        self.size = size
+        self.scale = scale
     }
 
 }
