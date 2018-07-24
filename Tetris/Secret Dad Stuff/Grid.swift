e2171c0009743e9e8caf8c4b9ea6aa6e9ecd9f97
Drew Olbrich <milpitas@users.noreply.github.com>
2018-07-23 17:27:07 -0700

    Add Grid.swift

---
+++ Tetris/Secret Dad Stuff/Grid.swift
@@ -0,0 +1,71 @@
+
+class Grid {
+
+    public let matrix: Matrix
+    
+    init(cellsX: Int, cellsY: Int, center: CGPoint, scale: GGFloat) {
+        matrix = Matrix(cellsX: cellsX, cellsY: cellsY, center: center, scale: scale)
+        matrix.layoutCells()
+    }
+    
+    deinit {
+        matrix.removeCells()
+    }
+    
+    /// Assigns a background texture that appears in each cell when it is cleared
+    /// with clearTexture, or with setTexture with an argument of 0.
+    public func setBackgroundTexture(_ backgroundTexture: Int) {
+        createTextureAtlasControllerIfNecessary()
+    
+        matrix.defaultTextureIndex = backgroundTexture
+    }
+
+    /// Sets a texture at a grid cell. If 0 is specified for the texture,
+    /// the cell's texture is cleared.
+    public func setTexture(_ index: Int, x: Int, y: Int) {
+        createTextureAtlasControllerIfNecessary()
+
+        let cell = matrix.cellAt(x: x, y: y)
+        if cell == nil {
+            return
+        }
+        if index == 0 {
+            // This matches the behavior of clearTexture.
+            cell?.textureIndex = nil
+        } else {
+            cell?.textureIndex = index
+        }
+    }
+
+    /// Clears the texture at a grid cell.
+    public func clearTexture(x: Int, y: Int) {
+        let cell = matrix.cellAt(x: x, y: y)
+        if cell == nil {
+            return
+        }
+        cell?.textureIndex = nil
+    }
+
+    /// Returns true if a texture exists at a grid cell.
+    public func hasTextureAt(x: Int, y: Int) -> Bool {
+        guard let cell = matrix.cellAt(x: x, y: y) else {
+            return false
+        }
+
+        return cell.textureIndex != nil
+    }
+
+    /// Returns the texture at a grid cell. Returns 0 if the cell has no texture.
+    public func textureAt(x: Int, y: Int) -> Int {
+        guard let cell = matrix.cellAt(x: x, y: y) else {
+            return 0
+        }
+
+        guard let textureIndex = cell.textureIndex else {
+            return 0
+        }
+
+        return textureIndex
+    }    
+
+}
